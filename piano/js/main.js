b84d1d7fc0b4b5bdaf6a0202641b3e022ca73a80
dominada <90106258+dominada@users.noreply.github.com>
2022-03-27 09:06:41 +0200

    3

---
+++ piano/js/main.js
@@ -0,0 +1,33 @@
+var audio_C = new Audio('audio_file.mp3');
+var audio_D = new Audio('audio_file.mp3');
+var audio_E = new Audio('audio_file.mp3');
+var audio_F = new Audio('audio_file.mp3');
+var audio_G = new Audio('audio_file.mp3');
+var audio_A = new Audio('audio_file.mp3');
+var audio_B = new Audio('audio_file.mp3');
+var audio_C2 = new Audio('audio_file.mp3');
+
+document.getElementById("piano-button-1").addEventListener("click", ()=> {
+    console.log("C")
+});
+document.getElementById("piano-button-2").addEventListener("click", ()=> {
+    console.log("D")
+});
+document.getElementById("piano-button-3").addEventListener("click", ()=> {
+    console.log("E")
+});
+document.getElementById("piano-button-4").addEventListener("click", ()=> {
+    console.log("F")
+});
+document.getElementById("piano-button-5").addEventListener("click", ()=> {
+    console.log("G")
+});
+document.getElementById("piano-button-6").addEventListener("click", ()=> {
+    console.log("A")
+});
+document.getElementById("piano-button-7").addEventListener("click", ()=> {
+    console.log("B")
+});
+document.getElementById("piano-button-8").addEventListener("click", ()=> {
+    console.log("C")
+});
